cf7135a4ee5b514f0a6c047f7c4cd2906017449a
bwbr3 <113187276+bwbr3@users.noreply.github.com>
2022-09-11 11:18:26 -0500

    Add files via upload

---
+++ Comp Sci Program 1.py
@@ -0,0 +1,65 @@
+print('***APPLE PIE INGREDIENTS***')
+
+print()
+
+name = input('How many pies do you need ==>')
+
+print()
+
+pie = 1
+print('For',pie,'pie(s) you will need:')
+print()
+
+pie_crust = pie * 1
+apples = pie * 6
+sugar = pie * 0.75
+flour = pie * 2
+cinn = pie * 0.75
+salt = pie * 0.25
+nutmeg = pie * 0.125
+lem_juice = pie * 1
+
+print(pie_crust, 'Pie crust(s)')
+print(apples, 'Apples')
+print(sugar, 'cups of Sugar')
+print(flour, 'Tablespoon of Flour')
+print(cinn, 'Teaspoon of Cinnamon')
+print(salt, 'Teaspoon of Salt')
+print(nutmeg, 'Teaspoon of Nutmeg')
+print(lem_juice, 'Tablespoon of Lemon Juice')
+
+print()
+
+print('>>>')
+
+print()
+
+print('***APPLE PIE INGREDIENTS***')
+
+print()
+
+name = input('How many pies do you need ==>')
+
+print()
+
+pie = 2
+print('For',pie,'pie(s) you will need:')
+print()
+
+pie_crust = pie * 1
+apples = pie * 6
+sugar = pie * 0.75
+flour = pie * 2
+cinn = pie * 0.75
+salt = pie * 0.25
+nutmeg = pie * 0.125
+lem_juice = pie * 1
+
+print(pie_crust, 'Pie crust(s)')
+print(apples, 'Apples')
+print(sugar, 'cups of Sugar')
+print(flour, 'Tablespoon of Flour')
+print(cinn, 'Teaspoon of Cinnamon')
+print(salt, 'Teaspoon of Salt')
+print(nutmeg, 'Teaspoon of Nutmeg')
+print(lem_juice, 'Tablespoon of Lemon Juice')
